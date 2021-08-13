6ba1d63b5af2fc5bd65795059ab2d393d8d653b4
Paulo Zandamela <paulozanda@hotmail.com>
2021-08-13 19:56:07 +0200

    VERSAO 3.0 DA CALCULADORA

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+
+public class Calculadora{
+	public static void public static void main(String[] args) {
+		
+		int num1, num2, resolutado;
+
+		System.out.print("MEU PRIMEIRO PROGRAMA.");
+
+		System.out.print("CODIGO ATUALIZADO");
+	}
+}
---
+++ Calculadora_1.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+
+public class Calculadora_1{
+     public static void main(String [] args) throws IOException{
+
+        BufferedReader x  = new BufferedReader (new InputStreamReader(System.in));
+        double num1, num2, resolutado=0;
+        byte op;
+
+        System.out.print("Calculadora -  SELECIONE A OPÇÃO: \n1-SOMA \n2-SUBTRAÇÃO \n3-MULTIPLICAÇÃO \n4-DIVISÃO");
+        op=Byte.parseByte(x.readLine());
+
+        System.out.print(" INTRODUZA O PRIMEIRO VALOR: ");
+        num1 = Double.parseDouble(x.readLine());
+
+        System.out.print(" INTRODUZA O SEGUNDO VALOR: "); 
+        num2 = Double.parseDouble(x.readLine());
+
+        do{
+                switch (op) {
+                        case 1:
+                                resolutado  = num1 + num2;
+                                System.out.println("RESULTADO DO CALCULO: "+resolutado);
+                        break;
+
+                        case 2:
+                                resolutado  = num1 - num2;
+                                System.out.println("RESULTADO DO CALCULO: "+resolutado);
+                        break;
+
+                        case 3:
+                                resolutado  = num1 * num2;
+                                System.out.println("RESULTADO DO CALCULO: "+resolutado);
+                        break;
+
+                        case 4:{
+                                if(num2==0){
+                                        System.out.println("NAO É POSSÍVEL DIVIDIR UM NÚMERO ZERO");
+                                }
+                                else{
+                                        resolutado  = num1 + num2;
+                                        System.out.println("RESULTADO DO CALCULO: "+resolutado);
+                                }
+                        }
+                        break;
+
+                        default: 
+                                System.out.println("OPCAO INVALIDA...");
+                        break;
+                }
+        }while(op>=0 || op<=4);
+
+    }
+}
+
